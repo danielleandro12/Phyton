2a3416a486e1839b1d5a9ad683671489d97da08a
Daniel Leandro <166567188+danielleandro12@users.noreply.github.com>
2025-01-11 08:25:03 -0300

    Update funções.py

--- Aula04/funções.py
+++ Aula04/funções.py
@@ -1,89 +1,89 @@
-#LISTA ESTAGIARIOS
-# - Crie uma função mostrar_mensagem que não receba nenhum parâmetro e apenas imprima a mensagem "Olá, mundo!".
-#def mostrar_mensagem()
-#return f"Olá,mundo!"
-#print(mostrar_mensagem):
+LISTA ESTAGIARIOS
+Crie uma função mostrar_mensagem que não receba nenhum parâmetro e apenas imprima a mensagem "Olá, mundo!".
+def mostrar_mensagem()
+return f"Olá,mundo!"
+print(mostrar_mensagem):
 
-# - Crie uma função chamada saudar que recebe o nome de uma pessoa como parâmetro e retorna uma saudação personalizada (ex: "Olá, [nome]!").
-#def saudar(nome):
-#    return f"Seja bem vindo {nome}"
-#saudar ("leandro")   
-#Crie uma função soma que receba dois números como parâmetros e retorne a soma deles.
-#def somar(numero1:int,numero2:int):#Dica de tipo ou Type hint
-#    """
-#    numero1 -> recebe um numero inteiro
-#    numero2 -> recebe outro numero inteiro
-#    retorna a soma destes dois numeros
-#    """
-#    soma = numero1+numero2
-#    return soma
-#print(somar(2,8))
-#numero1 = int(input("Digite um número:"))
-#numero2 = int(input("Digite outro número:"))
-#print(numero1+numero2)
-# - Crie uma função multiplicar_tupla que recebe uma tupla de números e retorna o produto de todos os elementos da tupla.
-#        somaresultado*=
-#    return somaresultado
-#tupla =(7,8,9)    
-#print(multiplicar_tupla(7,8,9))
+Crie uma função chamada saudar que recebe o nome de uma pessoa como parâmetro e retorna uma saudação personalizada (ex: "Olá, [nome]!").
+def saudar(nome):
+return f"Seja bem vindo {nome}"
+saudar ("leandro")   
+Crie uma função soma que receba dois números como parâmetros e retorne a soma deles.
+def somar(numero1:int,numero2:int):#Dica de tipo ou Type hint
+    """
+    numero1 -> recebe um numero inteiro
+    numero2 -> recebe outro numero inteiro
+    retorna a soma destes dois numeros
+    """
+    soma = numero1+numero2
+    return soma
+print(somar(2,8))
+numero1 = int(input("Digite um número:"))
+numero2 = int(input("Digite outro número:"))
+print(numero1+numero2)
+ Crie uma função multiplicar_tupla que recebe uma tupla de números e retorna o produto de todos os elementos da tupla.
+        somaresultado*=
+    return somaresultado
+tupla =(7,8,9)    
+print(multiplicar_tupla(7,8,9))
 
-#LISTA JUNIORS
-# - Crie uma função contar_vogais que receba uma string e conte quantas vogais (a, e, i, o, u) existem nela.
-#lens = input('Digite uma Palavra: ')
-#vogal = []
+LISTA JUNIORS
+ Crie uma função contar_vogais que receba uma string e conte quantas vogais (a, e, i, o, u) existem nela.
+lens = input('Digite uma Palavra: ')
+vogal = []
 
-# Listas para facilitar a verificação
-#for l in lens:
-#    if l in vogais:
-#        vogal.append(l)
-#print(f'Esse texto tem {len(vogal)} vogais')
+Listas para facilitar a verificação
+for l in lens:
+    if l in vogais:
+    vogal.append(l)
+print(f'Esse texto tem {len(vogal)} vogais')
 
-# - Crie uma função chamada contagem_regressiva que recebe um número n e imprime os números de n até 1, em contagem regressiva.
-#def contagem_regressiva(numero)
+Crie uma função chamada contagem_regressiva que recebe um número n e imprime os números de n até 1, em contagem regressiva.
+def contagem_regressiva(numero)
     
 
 
-# - Crie uma função calcular_imc que receba o peso e altura de uma pessoa e calcule o Índice de Massa Corporal (IMC), com valores padrões de peso 70 kg e altura 1.75 m.
+ Crie uma função calcular_imc que receba o peso e altura de uma pessoa e calcule o Índice de Massa Corporal (IMC), com valores padrões de peso 70 kg e altura 1.75 m.
 
 
 
-# - Crie uma função chamada media que receba três notas de um aluno e calcule e retorne a média delas.
+ Crie uma função chamada media que receba três notas de um aluno e calcule e retorne a média delas.
 
 
 
-# - Crie uma função par_ou_impar que receba um número e retorne uma string informando se o número é "par" ou "ímpar".
+ Crie uma função par_ou_impar que receba um número e retorne uma string informando se o número é "par" ou "ímpar".
 
 
 
-#LISTA PLENO
-# - Crie uma função chamada calculadora que receba três parâmetros: dois números e uma operação (como uma string: "soma", "subtração", "multiplicação", "divisão"). A função deve retornar o resultado da operação escolhida entre os dois números.
+LISTA PLENO
+ Crie uma função chamada calculadora que receba três parâmetros: dois números e uma operação (como uma string: "soma", "subtração", "multiplicação", "divisão"). A função deve retornar o resultado da operação escolhida entre os dois números.
 
 
 
-# - Crie uma função maior_numero que receba uma lista de números e retorne o maior número dessa lista.
+ Crie uma função maior_numero que receba uma lista de números e retorne o maior número dessa lista.
 
 
 
-# - Crie uma função eh_palindromo que recebe uma string e retorna True se ela for um palíndromo (ou seja, se pode ser lida da mesma forma de trás para frente EXEMPLO NATAN), e False caso contrário.
+ Crie uma função eh_palindromo que recebe uma string e retorna True se ela for um palíndromo (ou seja, se pode ser lida da mesma forma de trás para frente EXEMPLO NATAN), e False caso contrário.
 
 
 
-# - Crie uma função desconto que receba o preço de um produto e um percentual de desconto (opcional, com valor padrão de 10%). A função deve retornar o preço com o desconto aplicado.
+ Crie uma função desconto que receba o preço de um produto e um percentual de desconto (opcional, com valor padrão de 10%). A função deve retornar o preço com o desconto aplicado.
 
 
 
-#LISTA SENIOR
-#  - Crie uma função recursiva chamada fatorial que calcule o fatorial de um número.
+LISTA SENIOR
+ Crie uma função recursiva chamada fatorial que calcule o fatorial de um número.
 
 
 
-#- Crie uma função que receba um numero inteiro como parâmetro e retorna a sequencia fibonnaci ate aquela ordem respectiva.
+ Crie uma função que receba um numero inteiro como parâmetro e retorna a sequencia fibonnaci ate aquela ordem respectiva.
 
 
 
-#DESAFIO
-# Faça um gerador de senhas aleatórias , que receba como parâmetros o tamanho da senha , que por padrão inicia em 6
+DESAFIO
+Faça um gerador de senhas aleatórias , que receba como parâmetros o tamanho da senha , que por padrão inicia em 6
 
 
 
-# Faça um descobridor de senhas de 4 digitos .(Enunciado é curto mais o problema é longo)Use a criatividade !
+Faça um descobridor de senhas de 4 digitos .(Enunciado é curto mais o problema é longo)Use a criatividade !
