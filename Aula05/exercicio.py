6a05f767dc24387da3ee161089110f946c074051
Daniel Leandro <166567188+danielleandro12@users.noreply.github.com>
2025-01-18 10:28:36 -0300

    Add files via upload

---
+++ Aula05/exercicio.py
@@ -0,0 +1,39 @@
+import random
+
+print('Seja bem vindo ao jogo de pedra, papel e tesoura!')
+while True:
+    print('Regras:')
+    print('Pedra ganha de tesoura')
+    print('Papel ganha de pedra')
+    print('Tesoura ganha de papel')
+    print('Digite sair a qualquer momento para encerrar')
+
+    opcao1 = ("Pedra")
+    opcao2 = ("Tesoura")
+    opcao3 = ("Papel")
+    escolha = input('Escolha a sua opção:')
+
+    opcoes = ["Pedra" , "Papel" ,"Tesoura"]
+    print(random.choice(opcoes))
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+    
+
+
+        
+
+
